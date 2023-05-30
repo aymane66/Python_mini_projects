e7a99f38013ff65c08e7f60b02e15182e49b8e08
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-05-30 16:52:21 +0100

    Basics of Py

--- Turnover.py
+++ Turnover.py
@@ -2,4 +2,4 @@ sales = int(input("Quantity of products sold: "))
 price = float(input("Price of each item: "))
 
 turnover = sales * price
-print("Turnover is: ", turnover)
+print("Turnover is: ", turnover, "MAD")
