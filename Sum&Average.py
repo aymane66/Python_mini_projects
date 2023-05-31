aba0d066e087b59a77b6380210740d4d0dfcf225
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-05-31 23:48:12 +0100

    Basics of Py

---
+++ Sum&Average.py
@@ -0,0 +1,9 @@
+a = float(input("Insert first number: "))
+b = float(input("Insert second number: "))
+c = float(input("Insert third number: "))
+d = float(input("Insert fourth number: "))
+e = float(input("Insert fifth number: "))
+
+av = (a + b + c + d + e) / 5
+
+print("Average: ", format(av, ".2f"))
