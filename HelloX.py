337e0dc7a8217ccecddf29d0ada34b310b44a109
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-05-31 22:01:37 +0100

    Basics of Py

---
+++ HelloX.py
@@ -0,0 +1,3 @@
+print("Hello world")
+x = input("What is your name? ")
+print("Hello ", x, "I hope you are doing well today.")
---
+++ RectangleArea.py
@@ -0,0 +1,10 @@
+print("Let´s calculate the area and perimeter of a rectangle")
+width = float(input("Insert the width of the rectangle: "))
+length = float(input("Insert the length of the rectangle: "))
+
+area = width * length
+perimeter = 2 * (length + width)
+
+print("----- Result -----")
+print("Area of the rectangle: ", area)
+print("Perimeter of the rectangle: ", perimeter)
