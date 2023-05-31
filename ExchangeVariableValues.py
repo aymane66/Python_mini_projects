41b4bac1bf585484997e3936600cf336c4b0b4f6
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-06-01 00:12:28 +0100

    Basics of Py

---
+++ ExchangeVariableValues.py
@@ -0,0 +1,10 @@
+x = int(input("Insert value of x: "))
+y = int(input("Insert value of y: "))
+
+tmp = x
+x = y
+y = tmp
+
+print("----- Value exchanged -----")
+print("Value of x: ", x)
+print("Value of y: ", y)
