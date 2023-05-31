ef0d6e364996051361d3075e510a8b096e907d41
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-05-31 21:33:26 +0100

    Basics of Py

---
+++ WelcomeUser.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = int (input("How old are you? "))
+
+print("Welcome back", name, ".", "You are", age, "years old.", "Welcome at the university's main page.")
