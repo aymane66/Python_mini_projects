d9b0f4b8ca3d75441c93384fb89c5e853f70222e
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-05-31 23:18:39 +0100

    Basics of Py

---
+++ BasicMathOperations.py
@@ -0,0 +1,12 @@
+x = float(input("Insert the first number: "))
+y = float(input("Insert the second number: "))
+
+sum = x + y
+sub = x - y
+pro = x * y
+div = x / y
+print("Sum: ", format(sum,".2f"))
+print("Subtraction: ", format(sub,".2f"))
+print("Product: ", format(pro,".2f"))
+print("Division: ", format(div,".2f"))
+
