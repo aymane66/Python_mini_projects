3f36986bfd6cd6d607cc853c883229526a14a75d
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-06-01 15:51:05 +0100

    Basics of Py

--- GrossPay.py
+++ GrossPay.py
@@ -1,6 +1,10 @@
 hours = int(input("Enter Hours: "))
 rate = float(input("Enter rate: "))
 
-pay = hours * rate
+if hours > 40:
+    overtime = hours - 40
+    pay = (hours * rate) + (overtime * (rate * 0.5))
+else:
+    pay = hours * rate
 
 print("Pay: ", format(pay, ".2f"))
