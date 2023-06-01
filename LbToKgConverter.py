68803afb771176ef6f7fd9c63a93233d7e347a87
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-06-01 19:31:40 +0100

    Basics of Py

---
+++ LbToKgConverter.py
@@ -0,0 +1,18 @@
+weight = input("Please enter your weight: ")
+unit = input("Select the unit of measurement (K = kg, L = lbs): ")
+
+try:
+    weight = float(weight)
+
+    if unit.lower() == "k":
+        r = weight * 2.205
+        print("Your weight in pounds is: ", format(r, ".2f"), "lbs")
+
+    elif unit.lower() == "l":
+        r = weight / 2.205
+        print("Your weight in kilograms is: ", format(r, ".2f"), "kg")
+    else:
+        print("Error: Invalid unit of measurement. Please select either 'K' or 'L'.")
+
+except:
+    print("Invalid input. Please enter a number for weight.")
