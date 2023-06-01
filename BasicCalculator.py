eb50ad5d959bc027bcd42541da7d8fc3aeb9af9c
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-06-01 18:42:13 +0100

    Basics of Py

---
+++ BasicCalculator.py
@@ -0,0 +1,30 @@
+x = input("x: ")
+op = input("operator: ")
+y = input("y: ")
+
+try:
+    x = float(x)
+    y = float(y)
+
+    if op == "+":
+        r = x + y
+        print("Result: ", format(r, ".2f"))
+    elif op == "-":
+        r = x - y
+        print("Result: ", format(r, ".2f"))
+    elif op == "*":
+        r = x * y
+        print("Result: ", format(r, ".2f"))
+    elif op == "/":
+        if y != 0:
+            r = x / y
+            print("Result: ", format(r, ".2f"))
+        else:
+            print("Error. Division by zero is not allowed.")
+    else:
+        print("Invalid operator. Please enter one of '+', '-', '*', or '/'.")
+
+except:
+    print("Invalid input. Please enter numbers for x and y.")
+
+
