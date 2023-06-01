aa04609e7991546c5b3d433bc61f8ad65a91a29d
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-06-01 15:20:57 +0100

    Basics of Py

---
+++ Width&Height.py
@@ -0,0 +1,12 @@
+width = 17
+height = 12.0
+
+a = width//2
+b = width/2.0
+c = height/3
+d = 1 + 2 * 5
+
+print("width//2 = ", a)
+print("width/2.0 = ", b)
+print("height/3 = ", c)
+print("1 + 2 * 5 = ", d)
