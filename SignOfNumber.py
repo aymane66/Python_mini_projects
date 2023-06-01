12c612489727add1d1b7c4f45fbca9836e348f3d
aymane661~ <ayman.mouhcine1995@gmail.com>
2023-06-01 22:55:28 +0100

    Basics of Py

---
+++ SignOfNumber.py
@@ -0,0 +1,14 @@
+n = input("Insert a number: ")
+
+try:
+    n = float(n)
+
+    if n < 0:
+        print("Negative number.")
+    elif n > 0:
+        print("Positive number.")
+    else:
+        print("Zero.")
+
+except ValueError:
+    print("Error. Please insert a numerical input. ")
