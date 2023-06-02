4d08b7bd577c82b8b020825b7746406c894f3cbb
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-02 11:07:54 +0100

    Basics of Py

---
+++ CarGame.py
@@ -0,0 +1,19 @@
+command = ""
+
+while True:
+    command = input("Command -> ").lower()
+    if command == "help":
+        print("""
+        start - to start the car
+        stop - to stop the car
+        quit - to exit
+        """)
+    elif command == "start":
+        print("Car started ... Ready to go!")
+    elif command == "stop":
+        print("Car stopped!")
+    elif command == "quit":
+        quit()
+    else:
+        print("Error. Wrong command")
+
