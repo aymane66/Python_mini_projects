335fcfb8907b3778d2aa439348a401fcc29b39c7
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-03 19:58:18 +0100

    Basics of Py

--- TaxRegulations.py
+++ TaxRegulations.py
@@ -4,9 +4,12 @@ gender = input("Insert your gender (F- Female M - Male): ")
 try:
     age = int(age)
 
-    if gender.lower == "m" and age > 20:
+    if gender.lower() == "m" and age > 20:
         print("You are eligible to pay taxes")
-    elif (gender.lower == "f") and (age >= 18 and age <= 35):
+    elif gender.lower() == "f" and 18 <= age <= 35:
         print("You are eligible to pay taxes")
     else:
         print("You are not eligible to pay taxes.")
+
+except ValueError:
+    print("Invalid input.")
