a8c495bf6dfeb8e0db8cfacd756d60e7bca89386
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-03 22:07:16 +0100

    Basics of Py

---
+++ GameController.py
@@ -0,0 +1,18 @@
+command = input("Command: ")
+
+try:
+    command = int(command)
+
+    if command == 6:
+        print("The character goes right. ")
+    elif command == 4:
+        print("The character goes left. ")
+    elif command == 8:
+        print("The character goes up. ")
+    elif command == 2:
+        print("The character goes down. ")
+    else:
+        print("Wrong command. ")
+
+except ValueError:
+    print("Invalid input. ")
