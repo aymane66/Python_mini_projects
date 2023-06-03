1afc60cca93e3674dd7d1603438362837df27f9a
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-03 07:15:35 +0100

    Basics of Py

--- CostWithDiscounts.py
+++ CostWithDiscounts.py
@@ -4,15 +4,15 @@ try:
     copies = int(copies)
     if copies <= 10:
         cost = copies * 0.30
-        print("Cost: ", format(cost, ".2f"))
+        print("Cost: ", format(cost, ".2f"), "MAD")
 
     elif copies > 30:
         cost = 10 * 0.30 + 20 * 0.25 + (copies - 30) * 0.20
-        print("Cost: ", format(cost, ".2f"))
+        print("Cost: ", format(cost, ".2f"), "MAD")
 
     else:
         cost = 10 * 0.30 + (copies - 10) * 0.25
-        print("Cost: ", format(cost, ".2f"))
+        print("Cost: ", format(cost, ".2f"), "MAD")
 
 except ValueError:
     print("Error. Please insert a numerical value. ")
