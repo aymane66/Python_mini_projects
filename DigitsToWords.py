f2edee5f026421926fd3619b143369e8c7b58cfd
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-04 11:18:07 +0100

    Basics of Py

---
+++ DigitsToWords.py
@@ -0,0 +1,17 @@
+numbers = input("Insert numbers in digits (0 to 9): ")
+
+dictionary = {
+    "0": "Zero ",
+    "1": "One ",
+    "2": "Two ",
+    "3": "Three ",
+    "4": "Four ",
+    "5": "Five ",
+    "6": "Six ",
+    "7": "Seven ",
+    "8": "Eight ",
+    "9": "Nine ",
+    }
+
+for i in numbers:
+    print(dictionary[i], end="")
