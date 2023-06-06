27ab911a41976e226ca9c2023abf5437de3615d7
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-06 22:13:33 +0100

    For and while loops

---
+++ Random_number_finder.py
@@ -0,0 +1,22 @@
+import random
+print("Hey! I chose a number between 1 and 30. Can you guess it? ")
+print("You have 5 trials!")
+print("-----------------------------")
+n = random.randint(1, 30)
+g = 0
+t = 0
+
+while t < 6:
+    g = int(input("Guess the number: "))
+    t += 1
+
+    if g == n:
+        print(f"Correct! You found {n} in {t} trials. ")
+        break
+    elif g < n:
+        print("Hint: It´s a bigger number. ")
+
+    else:
+        print("Hint: It´s a smaller number. ")
+else:
+    print("I am sorry. You lost the game. The secret number was", n)
