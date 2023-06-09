a3b6bff20edf195906c5f31c75f1a87d6b62bda6
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-09 15:00:21 +0100

    For and while loops

---
+++ Shape_Diamond.py
@@ -0,0 +1,14 @@
+n = input("Number: ")
+
+try:
+    n = int(n)
+
+    for i in range(1, n + 1):
+        for j in range(1, n - i + 1):
+            print("  ", end="")
+        for j in range(1, n + 1):
+            print("* ", end="")
+        print()
+
+except ValueError:
+    print("Invalid input! ")
