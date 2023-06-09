1960534da39d6ea2611d00b3906c1a97498a4afc
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-09 23:30:03 +0100

    For and while loops

---
+++ List_fruit_or_not_fruit.py
@@ -0,0 +1,7 @@
+items = ["steak", "apple", "bread", "butter", "pineapple"]
+
+for i in items:
+    if i == "apple" or i == "pineapple":
+        print(i, " is a fruit. ")
+    else:
+        print(i, " is NOT a fruit. ")
