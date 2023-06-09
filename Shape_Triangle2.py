d1d0f73aa57d492688e877272ebb267e578c711f
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-09 15:18:12 +0100

    For and while loops

---
+++ Shape_Triangle2.py
@@ -0,0 +1,15 @@
+n = input("Number: ")
+
+try:
+    n = int(n)
+
+    for i in range(1, n + 1):
+        for j in range(1, n - i + 1):
+            print("  ", end="")
+
+        for j in range(1, 2 * i - 1 + 1):
+            print("* ", end="")
+        print()
+
+except ValueError:
+    print("Invalid input! ")
