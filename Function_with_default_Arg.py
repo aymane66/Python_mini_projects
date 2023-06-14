3790065a9698b7c7d051e3af95226bb581c11cb5
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-14 19:01:10 +0100

    Functions

---
+++ Function_with_default_Arg.py
@@ -0,0 +1,6 @@
+def employee(name, salary=3000):
+    print(f"The salary of employee {name} is {salary} MAD")
+
+
+employee("Ayman")  # In this case, I did not mention the salary, but the program assigned the default salary to the name.
+employee("Robert", 5600)
