83985db7856b1f7374b148e9ca68135b1f001796
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-14 23:32:50 +0100

    Functions

--- A_dice_game.py
+++ A_dice_game.py
@@ -1,15 +1,11 @@
 from random import randint
 
-print("----------------- Dice simulator game -----------------")
 win_counter = 0
 
-for i in range(1001):
+for i in range(100001):
     x = randint(1, 6)
     if x == 2 or x == 5:
-        print("You win!! ")
         win_counter += 1
-    else:
-        print("You lost! ")
 
-print("----------------------- Score -----------------------------")
-print(f"You won {win_counter} times out of 1000 tosses. ")
+print("----------------------- Dice toss simulator -----------------------------")
+print(f"You won {win_counter} times out of 100000 tosses. ")
