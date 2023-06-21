48a66d2c42bf7473c9bcc4227bbea2e133986a4c
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-21 09:34:55 +0100

    Modules

--- Radnom_profile_generator.py
+++ Radnom_profile_generator.py
@@ -7,7 +7,6 @@ print("Age: ", fake.random_int(min=18, max=99))
 print("Job: ", fake.job())
 print("Address: ", fake.address())
 print("City: ", fake.city())
-print("Country: ", fake.country() )
+print("Country: ", fake.country())
 print("Phone: ", fake.phone_number())
 print("Email: ", fake.email())
-
