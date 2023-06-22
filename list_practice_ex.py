66ffb2e74212841c6ddf1537587923cd151db59a
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-22 08:33:13 +0100

    Lists

---
+++ list_practice_ex.py
@@ -0,0 +1,5 @@
+days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+print(days_of_the_week[0])
+print(days_of_the_week[-2:])
+print(days_of_the_week[0:3])
