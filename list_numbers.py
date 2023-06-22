b9b8232c62918825cecefcc5e786dfc435b261ec
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-22 09:10:09 +0100

    Lists

--- list_numbers.py
+++ list_numbers.py
@@ -3,18 +3,16 @@ number = input("Please insert a positive number: ")
 try:
     number = int(number)
     if number < 0:
-        print("Please insert a positive number! ")
+        print("Please insert a positive number!")
     else:
         numbers = list(range(1, number + 1))
-        print("Numbers displayed: ", numbers)
+        print("Numbers displayed:", numbers)
 
         even_numbers = list(range(2, number + 1, 2))
-        print("Even numbers: ", even_numbers)
+        print("Even numbers:", even_numbers)
 
         odd_numbers = list(range(1, number + 1, 2))
-        print("Odd numbers: ", odd_numbers)
+        print("Odd numbers:", odd_numbers)
 
 except ValueError:
-    print("Invalid input! Please insert a number. ")
-
-
+    print("Invalid input! Please insert a number.")
