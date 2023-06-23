5dd67f789e94cdb7b62a5a0ad2e5111eecaa6f38
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-23 11:42:48 +0100

    Lists

--- list_content_calculations.py
+++ list_content_calculations.py
@@ -1,16 +1,15 @@
 numbers = []
 print("Insert 10 numbers: ")
 for i in range(10):
-    num = int(input(f"Number {i + 1}: "))
+    num = float(input(f"Number {i + 1}: "))
     numbers.append(num)
 
-numbers_list = numbers.copy()
-print(numbers_list)
+print(numbers)
 
-print("Total: ", sum(numbers_list))
-print("Average: ", sum(numbers_list) / len(numbers_list))
+print("Total: ", sum(numbers))
+print("Average: ", sum(numbers) / len(numbers))
 
 product = 1
-for i in numbers_list:
+for i in numbers:
     product *= i
 print("Product: ", product)
