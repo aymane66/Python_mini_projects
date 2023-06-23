2c2da6f69f112c40a7a00570e26d8d6819bc3d2e
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-23 11:27:13 +0100

    Lists

--- list_content_calculations.py
+++ list_content_calculations.py
@@ -1,17 +1,16 @@
 numbers = []
-count = int(input("How many numbers do you want to insert? "))
-
-for i in range(count):
+print("Insert 10 numbers: ")
+for i in range(10):
     num = int(input(f"Number {i + 1}: "))
     numbers.append(num)
-print("Numbers entered:", numbers)
 
-total = sum(numbers)
-average = total / len(numbers)
-print("Total:", total)
-print("Average: {:.2f}".format(average))
+numbers_list = numbers.copy()
+print(numbers_list)
 
-print("Product: ")
+print("Total: ", sum(numbers_list))
+print("Average: ", sum(numbers_list) / len(numbers_list))
 
-
-# Complete after finishing the course
+product = 1
+for i in numbers_list:
+    product *= i
+print("Product: ", product)
