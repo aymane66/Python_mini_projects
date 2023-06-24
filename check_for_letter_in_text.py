94e84240535a1b85c1c2b2de8ae30dc0f009cf3d
aymane66 <ayman.mouhcine1995@gmail.com>
2023-06-24 15:10:45 +0100

    Lists

--- check_for_letter_in_text.py
+++ check_for_letter_in_text.py
@@ -1,6 +1,10 @@
 text = list(input("Insert a sentence: "))
 letter = input("Count occurrences of letter: ")
 
+#Method 1:
+#count = text.count(letter)
+
+#Method 2:
 count = 0
 for char in text:
     if letter.lower() == char.lower():
